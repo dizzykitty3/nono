9b232c14eb43b41788fba229886cf1d3f682ef47
dizzykitty3 <130880627+dizzykitty3@users.noreply.github.com>
2026-09-17 19:03:38 +0800

    Initial Commit

---
+++ NONO/NONOApp.swift
@@ -0,0 +1,17 @@
+//
+//  NONOApp.swift
+//  NONO
+//
+//  Created by Theo on 9/17/26.
+//
+
+import SwiftUI
+
+@main
+struct NONOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NONOTests/NONOTests.swift
@@ -0,0 +1,19 @@
+//
+//  NONOTests.swift
+//  NONOTests
+//
+//  Created by Theo on 9/17/26.
+//
+
+import Testing
+@testable import NONO
+
+struct NONOTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
